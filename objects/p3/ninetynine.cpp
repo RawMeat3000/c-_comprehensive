06b6abecff926f52e31e73a7385ccc88a5df7ae9
RawMeat3000 <rawmeat3000@gmail.com>
2019-10-14 23:39:29 -0700

    new project full of beer on the wall

---
+++ objects/p3/ninetynine.cpp
@@ -0,0 +1,44 @@
+
+
+void run()
+{
+	const char* of_beer = "of beer on the wall,";
+	const char* bottle_single = "bottle";
+	const char* bottle_plural = "bottles";
+	const char* zero_to_nine[] = {
+		"",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	};
+	const char* teens[] =
+	{
+		"ten",
+		"eleven",
+		"twelve",
+		"thirteen",
+		"fourteen",
+		"fifteen",
+		"sixteen",
+		"seventeen",
+		"eightteen",
+		"nineteen"
+	};
+	const char* tens[] =
+	{
+		"twenty",
+		"thirty",
+		"fourty",
+		"fifty",
+		"sixty",
+		"seventy",
+		"eighty",
+		"ninety"
+	}
+}
---
+++ objects/p3/ninetynine.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class ninetynine {
+public:
+	void run();
+private:
+};
