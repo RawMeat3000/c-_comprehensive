7ede636b28f906f8f3d4546c945b923c3425b094
RawMeat3000 <rawmeat3000@gmail.com>
2019-10-16 23:17:41 -0700

    done with beer

--- objects/p3/ninetynine.cpp
+++ objects/p3/ninetynine.cpp
@@ -1,12 +1,20 @@
+#include "../util/util.h"
+#include "ninetynine.h"
 
 
-void run()
+void print_bottles(int num_bottles, bool wall, bool second_line)
 {
-	const char* of_beer = "of beer on the wall,";
-	const char* bottle_single = "bottle";
-	const char* bottle_plural = "bottles";
-	const char* zero_to_nine[] = {
-		"",
+	const char* const of_beer = "of beer";
+	const char* const on_the_wall = "on the wall";
+	const char* const bottle = "bottle";
+	const char const space = ' ';
+	const char const ess = 's';
+	const char const hyphen = '-';
+
+	const char* const take_one_down = "take one down, pass it around";
+	const char* const end_line = ",\n";
+	const char* const zero_to_nine[] = {
+		"zero",
 		"one",
 		"two",
 		"three",
@@ -17,7 +25,7 @@ void run()
 		"eight",
 		"nine",
 	};
-	const char* teens[] =
+	const char* const teens[] =
 	{
 		"ten",
 		"eleven",
@@ -30,8 +38,10 @@ void run()
 		"eightteen",
 		"nineteen"
 	};
-	const char* tens[] =
+	const char* const tens[] =
 	{
+		NULL,
+		NULL,
 		"twenty",
 		"thirty",
 		"fourty",
@@ -40,5 +50,70 @@ void run()
 		"seventy",
 		"eighty",
 		"ninety"
+	};
+
+	int tens_place = 0;
+	int ones_place = 0;
+
+	tens_place = (num_bottles / 10);
+	ones_place = num_bottles % 10;
+
+	if ( num_bottles > 19 )
+	{
+		cout << tens[tens_place];
+		if ( ones_place )
+		{
+			cout << hyphen;
+			cout << zero_to_nine[ones_place];
+		}
+	}
+	else if ( 9 < num_bottles && num_bottles < 20 )
+	{
+		cout << teens[ones_place];
+	}
+	else if ( 0 < num_bottles && num_bottles < 10 )
+	{
+		cout << zero_to_nine[ones_place];
+	}
+	else if ( num_bottles == 0 )
+	{
+		cout << "..." << endl;
+		cout << "..." << endl;
+		cout << "We're out of beer!" << endl;
+		return;
+	}
+
+	cout << space << bottle;
+
+	if ( num_bottles > 1 )
+		cout << ess;
+
+	cout << space << of_beer;
+
+	if ( !second_line )
+		cout << space << on_the_wall;
+
+	cout << end_line;
+
+	if ( second_line )
+		cout << take_one_down << end_line;
+}
+
+void ninetynine::run()
+{
+	int num_bottles = 99;
+
+	for ( ; num_bottles > 0; )
+	{
+		print_bottles(num_bottles, true, false);
+		print_bottles(num_bottles, false, true);
+
+		// minus for verse 2
+		num_bottles--;		
+
+		print_bottles(num_bottles, true, false);
+
+		cout << endl;
 	}
 }
+
